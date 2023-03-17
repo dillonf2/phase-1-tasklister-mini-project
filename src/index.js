c6cd70b094d9913f0dfb0f4f7098e80812f975be
Frank Dillon <dillonf2@outlook.com>
2023-03-17 18:51:16 -0400

    tasklister mini project completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener(`submit`,(e)=>{
+    e.preventDefault()
+    console.log(e.target)
+    buildToDo(e.target[`new-task-description`].value)
+  })
 });
+function buildToDo(todo){
+  let li=document.createElement(`li`)
+  li.textContent=todo
+  document.querySelector(`#tasks`).appendChild(li)
+}
